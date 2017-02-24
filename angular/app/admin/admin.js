8c5e69a5c3a964ac746ac83e795cb3f08b5714d5
Vinod <vinodpolishetty145@gmail.com>
2017-02-25 02:23:13 +0530

    Added validations for station edit and created Static views for Zones and station types.

--- angular/app/admin/admin.js
+++ angular/app/admin/admin.js
@@ -5,13 +5,14 @@
         .module('londonZones.admin',[])
         .controller('adminController', adminController);
 
-	function adminController($scope, QueryService){
+	function adminController($scope, QueryService, $timeout){
         $scope.displayviewDetails = true;
         $scope.showEdit = false;
+        $scope.errors = {};
         $scope.alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         $scope.zoneDetails = [];
-
-        $scope.action = "Action";
+        $scope.requiredFiels = ['name', 'zone', 'address', 'postCode', 'stationType', 'longitude', 'latitude'];
+        //$scope.action = "Action";
         QueryService.query('GET', 'components/services/data/data.json').then(function(trainDetails){
         //QueryService.query('GET', 'trainDetails').then(function(trainDetails){
             $scope.totalZoneDetails = trainDetails.data;
@@ -23,6 +24,7 @@
             $scope.curPage = 0;
             $scope.zoneDetails = [];
             $scope.displayviewDetails = true;
+            $scope.stationDeactivated = false;
             angular.forEach($scope.totalZoneDetails, function(zone) {
                 if(zone.name.startsWith(letter)){
                     $scope.zoneDetails.push(zone);
@@ -35,15 +37,18 @@
             $scope.displayZone = details;
         };
 
-        $scope.diactivateZone = function(details){
+        $scope.diactivateZone = function(details, index){
             console.log(details);
-            $scope.action =  "Deactivate";
+            //$scope.action =  "Deactivate";
             
-            QueryService.query('GET', 'components/services/data/data.json').then(function(trainDetails){
-                $scope.totalZoneDetails = trainDetails.data;
-                $scope.zoneDetails = $scope.totalZoneDetails;
-                 
-             });
+            // QueryService.query('POST', 'deleteStation', {id: details.zone}).then(function(result){
+            //     console.log("delete station");
+            //  });
+            $scope.zoneDetails.splice(index, 1);
+            $scope.stationDeactivated = true;
+            $timeout(function(){
+                $scope.stationDeactivated = false
+            }, 1500);
         };
 
         $scope.showAllZones = function(){
@@ -59,13 +64,34 @@
         }
 
         $scope.saveStationDetails = function(station){
-            console.log(station);
-            $scope.showEdit = false;
-            $scope.zoneDetails[$scope.statonIndex] = station;
+            $scope.errors.stationDetails = {};
+            if(!station){
+                $scope.stationDetails = {};
+            }
+            angular.forEach($scope.requiredFiels, function(attr){
+                if(!$scope.stationDetails[attr]){
+                    $scope.errors.stationDetails[attr] = 'This field is required';
+                }
+            });
+            if(angular.equals({}, $scope.errors.stationDetails)){
+                $scope.showEdit = false;
+                if(station){
+                    $scope.zoneDetails[$scope.statonIndex] = station;
+                }else{
+                    $scope.zoneDetails.push(station);
+                }
+            }
         }
 
         $scope.cancelEdit = function(){
             $scope.showEdit = false;
+            $scope.errors = {};
+        }
+
+        $scope.hideErrorMsg = function (id, key) {
+            if($scope.errors[key] && $scope.errors[key][id]) {
+                $scope.errors[key][id] = false;
+            }
         }
     }
  })();
