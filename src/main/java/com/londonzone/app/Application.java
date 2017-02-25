0cd1d6641f808a401a9d8fbfe8bde58f606cef89
Rajesh <rkumar@equilar.com>
2017-02-25 11:22:11 +0530

    Added context /londonzone

--- src/main/java/com/londonzone/app/Application.java
+++ src/main/java/com/londonzone/app/Application.java
@@ -20,12 +20,13 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/*").allowedOrigins("*");
 			}
 		};
 	}
