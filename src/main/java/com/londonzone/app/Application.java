fa302e1946185d75aa7f9a8a93badf26e469fd86
Rajesh <rkumar@equilar.com>
2017-03-25 12:51:30 +0530

    changes for war file generation

--- src/main/java/com/londonzone/app/Application.java
+++ src/main/java/com/londonzone/app/Application.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -24,10 +26,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EntityScan(basePackages = "com.londonzone.domain")
 @EnableJpaRepositories(basePackages = { "com.londonzone.repositories" })
 @PropertySource(value={"classpath:application.properties"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 	
 	@Autowired
 	private Environment environment ;
@@ -40,6 +43,11 @@ public class Application {
 			}
 		};
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	    }
 	
 	@Bean
 	public JavaMailSender getMailSender() {
